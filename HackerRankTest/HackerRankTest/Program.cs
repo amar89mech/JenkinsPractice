cef1c394999fdf010dc1ee115c6f9f479345da58
amar89mech <46593003+amar89mech@users.noreply.github.com>
2019-01-23 12:16:25 +0530

    Add files via upload

---
+++ HackerRankTest/HackerRankTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRankTest
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static string findNumber()
+        {
+            return "Hello world";
+
+        }
+    }
+}
